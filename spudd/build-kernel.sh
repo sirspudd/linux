5ded4f652c091f111caa06ce9bc65ed0cce0a34b
Donald Carr <d@chaos-reins.com>
2024-03-23 13:30:12 -0700

    Add spudd config, scripts and minor aggregate tweaks

---
+++ spudd/build-kernel.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o pipefail
+set -o errexit
+#set -o xtrace
+
+executedFile="${BASH_SOURCE[0]}"
+scriptDirectory=$(dirname $(realpath "${executedFile}"))
+rootDirectory=$(dirname "${scriptDirectory}")
+
+build_dir=/vortex/linux
+work_dir=${build_dir}/work
+mod_install_path=${work_dir}/modules_install
+deploy_path=${work_dir}/deploy
+boot=${deploy_path}/boot
+build_with_clang_args="LLVM=1 LLVM_IAS=1"
+make_flags="O=${build_dir} ${build_with_clang_args}"
+#make_flags="O=${build_dir}"
+
+rm -Rf ${build_dir}
+mkdir -p ${build_dir}
+
+echo "INSTALL_MOD_PATH=${mod_install_path} make ${make_flags} menuconfig"
+INSTALL_MOD_PATH=${mod_install_path} make ${make_flags} spudd_desktop_defconfig
+INSTALL_MOD_PATH=${mod_install_path} make ${make_flags} all
+INSTALL_MOD_PATH=${mod_install_path} make ${make_flags} modules_install
+#INSTALL_MOD_PATH=${mod_install_path} make ${make_flags} dtbs
